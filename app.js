2cfda7c31225e6d414ee8228be35cfb71b01eb8d
Reinkaos <ds2lvg@gmail.com>
2019-07-09 18:38:36 +0900

    [vtr.html] vanilla JS로 로 변경,[todo.js] mongoose 테스트

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require("express-session");
 const mongoose = require("mongoose");
+require('dotenv').config();
 // require router
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const socketsRouter = require('./routes/socket');
+const cors = require('cors');
 
 const app = express();
 
@@ -25,10 +27,23 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/img",express.static(__dirname+'/public/img'));
 app.use("/css",express.static(__dirname+'/public/css'));
 app.use("/js",express.static(__dirname+'/public/js'));
+
+mongoose.set('useCreateIndex', true);
+
+// Node.js의 native Promise 사용
+mongoose.Promise = global.Promise;
+
+// CONNECT TO MONGODB SERVER
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+.then(() => console.log('Successfully connected to mongodb'))
+.catch(e => console.error(e));
+
+app.use('/todos', require('./routes/todos'));
 
 // session
 app.use(
